f7da772a6d3deac544e1009bd8788de7291074ce
D3boker1 <fmarques_00@protonmail.com>
2024-08-13 10:58:18 +0100

    add(plat): add alsaqr platform
    Signed-off-by: D3boker1 <fmarques_00@protonmail.com>

---
+++ platform/alsaqr/inc/platform.h
@@ -0,0 +1,10 @@
+#ifndef PLATFORM_H
+#define PLATFORM_H
+
+#define MEM_BASE    (0x80000000)
+#define MEM_SIZE    (0x10000000)
+
+// Base address of the IOPMP Programming Interface
+#define IOPMP_BASE_ADDR            0x50020000ULL
+
+#endif
